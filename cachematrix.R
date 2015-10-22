4478269e2b5733c5cd0ada9f2a9ccb5c48184ea3
HansKok <nlv14116@NXL02262.wbi.nxp.com>
2015-10-22 20:43:45 +0200

    First release
    Need to add comment lines

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+    m_inv <- NULL
+    set <- function(m_in) {
+        m <<- m_in
+        m_inv <<- NULL
+    }
+    get <- function() m
+    set_inv <- function(m_inv_in) m_inv <<- m_inv_in
+    get_inv <- function() m_inv
+    list(set = set, get = get,
+         set_inv = set_inv,
+         get_inv = get_inv)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m_in, ...) {
+    m_inv <- m_in$get_inv()
+    
+    if(is.null(m_inv)) {
+        m = m_in$get()
+        m_inv = solve(m)
+        m_in$set_inv(m_inv)
+        m_inv
+        
+    } else {    
+        m_inv
+    }
 }
